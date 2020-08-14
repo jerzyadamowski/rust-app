40557d3309b5313cd00e8684ec88af4f79072e7d
Jerzy Adamowski <jerzy.adamowski@ringieraxelspringer.pl>
2020-08-15 00:23:02 +0200

    new lines

--- bin/rust-app.sh
+++ bin/rust-app.sh
@@ -14,14 +14,14 @@ do_any_cmd() {
 }
 
 do_show_help() {
-    printf "${GREEN}rust-app - application to install or execute rust tool with npx tool${NC}"
-    printf "${RED}invalid use${NC}"
-    printf "Usage: npx rust-app ${GREEN}[command]${NC}"
-    printf ""
-    printf "[command]: "
-    printf "      ${GREEN}rustup-init${NC} - run rust installer"
-    printf "      ${GREEN}rust-tool${NC} in ${GREEN}[cargo|cargo-fmt|clippy-driver|rustc|rustfmt|...]${NC}"
-    printf "Script takes in account all parameters eg. npx rust-app cargo --help : shows the help page"
+    printf "${GREEN}rust-app - application to install or execute rust tool with npx tool${NC}\n"
+    printf "${RED}invalid use${NC}\n"
+    printf "Usage: npx rust-app ${GREEN}[command]${NC}\n"
+    printf "\n"
+    printf "[command]: \n"
+    printf "\t${GREEN}rustup-init${NC} - run rust installer\n"
+    printf "\t${GREEN}rust-tool${NC} in ${GREEN}[cargo|cargo-fmt|clippy-driver|rustc|rustfmt|...]${NC}\n"
+    printf "Script takes in account all parameters eg. npx rust-app cargo --help : shows the help page\n"
 }
 
 case ${@:1:1} in
