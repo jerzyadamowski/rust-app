d74a0adf2555e95d2a235461d71f6740015d9e73
Jerzy Adamowski <jerzy.adamowski@ringieraxelspringer.pl>
2020-08-14 21:19:37 +0200

    functions order

--- bin/rust-app.sh
+++ bin/rust-app.sh
@@ -5,15 +5,6 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-case ${@:1:1} in
-	rustup-init)
-		do_rustup_init ;;
-	"")
-		do_show_help ;;
-	*)
-		do_any_cmd ;;
-esac
-
 do_rustup_init() {
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y "${@:2:$#}"
 }
@@ -32,4 +23,15 @@ do_show_help() {
     echo "      ${GREEN}rust-tool${NC} in ${GREEN}[cargo|cargo-fmt|clippy-driver|rustc|rustfmt|...]${NC}"
     echo "Script takes in account all parameters eg. npx rust-app cargo --help : shows the help page"
 }
+
+case ${@:1:1} in
+	rustup-init)
+		do_rustup_init ;;
+	"")
+		do_show_help ;;
+	*)
+		do_any_cmd ;;
+esac
+
+
 
