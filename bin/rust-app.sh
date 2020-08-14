c09c62e2c67ed3f02bd626200ecaf77dd1091e32
Jerzy Adamowski <jerzy.adamowski@ringieraxelspringer.pl>
2020-08-15 00:06:36 +0200

    print colors

--- bin/rust-app.sh
+++ bin/rust-app.sh
@@ -10,19 +10,18 @@ do_rustup_init() {
 }
 
 do_any_cmd() {
-    echo param
     source ~/.profile && "$@"
 }
 
 do_show_help() {
-    echo "${GREEN}rust-app - application to install or execute rust tool with npx tool${NC}"
-    echo "${RED}invalid use${NC}"
-    echo "Usage: npx rust-app ${GREEN}[command]${NC}"
-    echo ""
-    echo "[command]: "
-    echo "      ${GREEN}rustup-init${NC} - run rust installer"
-    echo "      ${GREEN}rust-tool${NC} in ${GREEN}[cargo|cargo-fmt|clippy-driver|rustc|rustfmt|...]${NC}"
-    echo "Script takes in account all parameters eg. npx rust-app cargo --help : shows the help page"
+    printf "${GREEN}rust-app - application to install or execute rust tool with npx tool${NC}"
+    printf "${RED}invalid use${NC}"
+    printf "Usage: npx rust-app ${GREEN}[command]${NC}"
+    printf ""
+    printf "[command]: "
+    printf "      ${GREEN}rustup-init${NC} - run rust installer"
+    printf "      ${GREEN}rust-tool${NC} in ${GREEN}[cargo|cargo-fmt|clippy-driver|rustc|rustfmt|...]${NC}"
+    printf "Script takes in account all parameters eg. npx rust-app cargo --help : shows the help page"
 }
 
 case ${@:1:1} in
